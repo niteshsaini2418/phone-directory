4cdeb7ae9fa2a44a15cc330bbab418984844dcb3
niteshsaini2418 <49919006+niteshsaini2418@users.noreply.github.com>
2019-12-04 23:54:55 +0530

    JSX vs. HTML: Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        Phone Directory
+        <div className="header">
+          Phone Directory
+        </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
 
       </div>
     );
