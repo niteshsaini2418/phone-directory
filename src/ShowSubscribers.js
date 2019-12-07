a46218bb23f5d7ead0f285d461f8ffabb79df984
niteshsaini2418 <49919006+niteshsaini2418@users.noreply.github.com>
2019-12-08 04:32:59 +0530

    Smart & Dumb: Added ‘PhoneDirectory’ as smart component, renamed ‘App…

--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,40 +1,36 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
-import './App.css';
+import './ShowSubscribers.css';
 
 class App extends Component {
-  constructor() {
+  constructor() {  
     super();
     this.state = {
       subscribersListToShow: []
     }
   }
-
   render() {
     return (
       <div>
-                <Header heading="Phone Directory" />
+        <Header heading="Phone Directory" />
         <div className="component-body-container">
           <button className="custom-btn add-btn">Add</button>
-
           <div className="grid-container heading-container">
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
           {
-              this.state.subscribersListToShow.map(sub => {
-
+            this.state.subscribersListToShow.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
                 <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn">Delete</button>
+                <button className="custom-btn delete-btn">Delete</button>
                 </span>
               </div>
             })
           }
         </div>
-
       </div>
     );
   }
